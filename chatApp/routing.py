5907f954c6f88917c262777712e21765aee19e08
Vivek-raj-gupta-2002 <vivekrajgupta2002@outlook.com>
2024-11-22 19:32:32 +0530

    update asgi.py

--- chatApp/routing.py
+++ chatApp/routing.py
@@ -1,8 +1,6 @@
 from django.urls import path, re_path
 from . import consumers
-import django
 
-django.setup()
 
 websocket_urlpatterns = [
     re_path(r"ws/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),  # Use as_asgi() for async consumers
