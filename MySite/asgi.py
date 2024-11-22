7f645b240a269b1bcf6a225375f5992d8390f0d0
Vivek-raj-gupta-2002 <vivekrajgupta2002@outlook.com>
2024-11-22 19:28:42 +0530

    update asgi.py

--- MySite/asgi.py
+++ MySite/asgi.py
@@ -1,14 +1,17 @@
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from chatApp import routing  # Import the routing from your chat app
 from channels.security.websocket import AllowedHostsOriginValidator
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MySite.settings')
+django.setup()
+application = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from chatApp import routing  # Import the routing from your chat app
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': application,
     'websocket': AllowedHostsOriginValidator(
         URLRouter(
             routing.websocket_urlpatterns
