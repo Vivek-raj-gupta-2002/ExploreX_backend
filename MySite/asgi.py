b9fe2f3cada01c294175f60cc642d1ac650e1813
Vivek-raj-gupta-2002 <vivekrajgupta2002@outlook.com>
2024-11-22 13:59:50 +0530

    changed manage.py

--- MySite/asgi.py
+++ MySite/asgi.py
@@ -4,12 +4,9 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chatApp import routing  # Import the routing from your chat app
 from channels.security.websocket import AllowedHostsOriginValidator
-import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MySite.settings')
 
-
-django.setup()
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
