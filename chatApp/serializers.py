9623066677ffd8da01df0407d4c35246e851865b
Vivek-raj-gupta-2002 <vivekrajgupta2002@outlook.com>
2024-11-24 14:18:14 +0530

    added undilivered messages

--- chatApp/serializers.py
+++ chatApp/serializers.py
@@ -12,6 +12,15 @@ class ChatSerializer(serializers.ModelSerializer):
     def get_last_message(self, obj):
         return obj.messages.last().content if obj.messages.exists() else None
 
+    def get_undelivered_message_count(self, obj):
+        # Get the user making the request
+        request = self.context.get('request')
+        if not request or not request.user:
+            return 0
+
+        # Count messages in this chat that are not sent by the user and have status 'sent'
+        return obj.messages.filter(status='sent').exclude(sender=request.user).count()
+
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
